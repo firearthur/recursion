d69c9df81ecc6e946250212fdc4a598ee5e85330
fireArthur <thefirearthur@gmail.com>
2018-01-01 16:46:47 -0800

    Implemented support for simple values arrays and objects still having trouble with complete support for objects

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -6,29 +6,124 @@
 // output: "{"x":5,"y":6}"
 
 //base case if obj is empty the return value is
-//empty object string '{' + jsonString + '}';
-
-//recursion case
-//create a string var jsonString = '';
 //create a copy of the passed in obj
 //var clonedObj = Object.assign(clonedObj, obj);
+//create a string var jsonParsedFilling = '';
+//empty object string '{' + jsonParsedFilling + '}';
+
+//recursion case
+
 //iterate over the keys in clonedObj using for in loop
 //in each loop
-//              jsonString += '"' + key + '"' + ':' + getPropertyValueInJsonFormat(clonedObj[key]);
+//              jsonParsedFilling += '"' + key + '"' + ':' + getPropertyValueInJsonFormat(clonedObj[key]);
 //              delete clonedObj[key];
 //              if(!isEmpty(clonedObj))
-//              jsonString += ',';
+//              jsonParsedFilling += ',';
 //              return stringifyJSON(clonedObj);
 
+// console.log(stringifyJSON([8,[[], 3, 4]]));
+
+//output '[8,[[],3,4]]'
 
 
 
-var stringifyJSON = function(obj) {
+function stringifyJSON(obj) {
   // your code goes here
+  //check for single values
+  var singleValue = stringifySingleValues(obj);
+  if(singleValue !== false){
+    return singleValue;
+  }
+
+  var jsonParsedFilling = '';
+
+  //check for arrays
+  if(Array.isArray(obj)){
+    jsonParsedFilling = stringifyArrays(obj);
+    return '[' + jsonParsedFilling + ']';
+    //
+    // if(obj.length < 1){
+    //   return '[' + jsonParsedFilling + ']';
+    // } else {
+    //   for (var i = 0; i < obj.length; i++) {
+    //     if(Array.isArray(obj[i])){
+    //       jsonParsedFilling += stringifyJSON(obj[i]);
+    //     } else {
+    //       jsonParsedFilling += stringifySingleValues(obj[i]);
+    //     }
+    //
+    //     if(!(i === obj.length - 1)){
+    //       jsonParsedFilling += ',';
+    //     }
+    //   }
+    //   return '[' + jsonParsedFilling + ']';
+    // }
+
+  }
+
+
+
+  //object
+  // var keys = Object.keys(obj);
+  // var values = Object.values(obj);
+  //
+  // if(keys.length < 1){
+  //   return '{' + jsonParsedFilling + '}';
+  // } else {
+  //   for(let i = 0; i < keys.length; i++){
+  //
+  //     if(typeof values[i] === 'object' && values[i] !== null){
+  //       jsonParsedFilling += stringifySingleValues(keys[i]) + ':';
+  //       jsonParsedFilling += stringifyJSON(obj[keys[i]]);
+  //
+  //     } else {
+  //       jsonParsedFilling += stringifySingleValues(keys[i]) + ':';
+  //       jsonParsedFilling += stringifySingleValues(values[i]);
+  //     }
+  //
+  //     if(!(i === keys.length - 1)){
+  //       jsonParsedFilling += ',';
+  //     }
+  //   }
+  //   return '{' + jsonParsedFilling + '}'
+  // }
+
+  jsonParsedFilling = stringifyObjects(obj);
+  return '{' + jsonParsedFilling + '}';
+
+
+
+  //
+  // if(isEmpty(obj) && typeof obj === 'object'){
+  //   stringifiedJsonObj = '{' + jsonParsedFilling + '}';
+  //   return stringifiedJsonObj;
+  // } else {
+  //   for (var key in obj) {
+  //     var clonedObj = Object.assign({}, obj);
+  //     if (clonedObj.hasOwnProperty(key)) {
+  //        jsonParsedFilling += '"' + key + '"' + ':' + getPropertyValueInJsonFormat(clonedObj[key]);
+  //        delete clonedObj[key];
+  //
+  //        if(isEmpty(clonedObj)){
+  //           // return '{' + jsonParsedFilling + '}';
+  //
+  //        } else {
+  //          jsonParsedFilling += ',';
+  //          return jsonParsedFilling += stringifyJSON(clonedObj);
+  //         // jsonParsedFilling += stringifyJSON(clonedObj);
+  //        }
+  //
+  //
+  //     }
+  //   }
+  //   return '{' + jsonParsedFilling + '}';
+  //
+  // }
+
 };
 
+//utility functions
 
-//utility function
 function isEmpty(obj) {
     for(var key in obj) {
         if(obj.hasOwnProperty(key))
@@ -36,11 +131,72 @@ function isEmpty(obj) {
     }
     return true;
 }
+
 function getPropertyValueInJsonFormat(value){
   if(typeof value === 'function' || typeof value === 'undefined'|| typeof value === 'symbol'){
     return null;
 
   } else {
-    return value;
+    return '"' + value + '"';
   }
+}
+
+function stringifySingleValues(value){
+  if(value === undefined){
+    return undefined;
+  } else if(typeof value === 'number' || value === null || typeof value === 'boolean'){
+    return String(value);
+  } else if(typeof value === 'string'){
+    return '"' + value + '"';
+  } else {
+    return false;
+  }
+}
+
+function stringifyObjects(obj){
+  var jsonParsedFilling = '';
+  var keys = Object.keys(obj);
+  var values = Object.values(obj);
+
+  if(keys.length < 1){
+    return jsonParsedFilling;
+  } else {
+    for(let i = 0; i < keys.length; i++){
+
+      if(typeof values[i] === 'object' && values[i] !== null){
+        jsonParsedFilling += stringifySingleValues(keys[i]) + ':';
+        jsonParsedFilling += stringifyJSON(obj[keys[i]]);
+
+      } else {
+        jsonParsedFilling += stringifySingleValues(keys[i]) + ':';
+        jsonParsedFilling += stringifySingleValues(values[i]);
+      }
+
+      if(!(i === keys.length - 1)){
+        jsonParsedFilling += ',';
+      }
+    }
+    return jsonParsedFilling;
+  }
+}
+
+function stringifyArrays(obj){
+  var jsonParsedFilling = '';
+  if(obj.length < 1){
+    return jsonParsedFilling;
+  } else {
+    for (var i = 0; i < obj.length; i++) {
+      if(Array.isArray(obj[i])){
+        jsonParsedFilling += stringifyJSON(obj[i]);
+      } else {
+        jsonParsedFilling += stringifySingleValues(obj[i]);
+      }
+
+      if(!(i === obj.length - 1)){
+        jsonParsedFilling += ',';
+      }
+    }
+    return jsonParsedFilling;
+  }
+
 }
